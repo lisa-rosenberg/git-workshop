8051160249163a483d17fa0d782221d09e3fc4e2
Lisa Rosenberg <lisa.rosenberg@msg.group>
2024-09-23 09:54:07 +0200

    Add lines for romaji data usage, Update docstrings

--- src/main/java/com/drandarov/hiragana/Scrambler.java
+++ src/main/java/com/drandarov/hiragana/Scrambler.java
@@ -14,10 +14,18 @@ public class Scrambler {
     /**
      * Run this method to print randomly generated syllable concatenations to the console.
      * Choose any amount of datasets from {@link HiraganaDataSet} in the first line.
+     * </br>
+     * If you want to use hiragana data, line 22+23 need to be commented in and line 26+27 need to be commented out.
+     * If you want to use romaji data, line 22+23 need to be commented out and line 26+27 need to be commented in.
      */
     public static void main(String[] args) {
+        // Use hiragana data
         String[][] dataSet = Stream.of(temp, hiraganaData, hiraganaExtraData)
                 .flatMap(Stream::of).toArray(String[][]::new);
+
+        // Use romaji data
+//        String[][] dataSet = Stream.of(temp, romajiData, romajiExtraData)
+//                .flatMap(Stream::of).toArray(String[][]::new);
 
         int minSyllableCount = 2; // e.g. "ba.ka"
         int maxSyllableCount = 5; // e.g. "ha.hi.fu.he.ho"
