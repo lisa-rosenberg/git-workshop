8a122ce63ff10060f1edbb3589d14b734d082474
Lisa Rosenberg <lisa.rosenberg@msg.group>
2024-09-23 09:10:15 +0200

    Add final, Reformat code

--- src/main/java/com/drandarov/hiragana/Scrambler.java
+++ src/main/java/com/drandarov/hiragana/Scrambler.java
@@ -15,21 +15,20 @@ public class Scrambler {
      * Run this method to print randomly generated syllable concatenations to the console.
      * Choose any amount of datasets from {@link HiraganaDataSet} in the first line.
      */
-    public static void main(String[] args) {
-        String[][] dataSet = Stream.of(temp, hiraganaData, hiraganaExtraData)
+    public static void main(final String[] args) {
+        final String[][] dataSet = Stream.of(temp, hiraganaData, hiraganaExtraData)
                 .flatMap(Stream::of).toArray(String[][]::new);
 
-        int minSyllableCount = 2; // e.g. "ba.ka"
-        int maxSyllableCount = 5; // e.g. "ha.hi.fu.he.ho"
-        int wordCount = 20;       // Amount of generated words
+        final int minSyllableCount = 2; // e.g. "ba.ka"
+        final int maxSyllableCount = 5; // e.g. "ha.hi.fu.he.ho"
+        final int wordCount = 20;       // Amount of generated words
         int set;
 
-        ThreadLocalRandom random = ThreadLocalRandom.current();
+        final ThreadLocalRandom random = ThreadLocalRandom.current();
 
         for (int i = 0; i < wordCount; i++) {
             for (int j = 0; j < random.nextInt(minSyllableCount, maxSyllableCount + 1); j++) {
-                System.out.print(dataSet[set = random.nextInt(0, dataSet.length)]
-                                              [random.nextInt(0, dataSet[set].length)]);
+                System.out.print(dataSet[set = random.nextInt(0, dataSet.length)][random.nextInt(0, dataSet[set].length)]);
             }
             System.out.println(/*"\n"*/); // Uncomment if you want empty lines between words
         }
